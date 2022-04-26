2d754071133fa2ff21aa2bd6d9688d40fc466124
ohusq <70851059+happyspeler@users.noreply.github.com>
2022-04-26 17:35:38 +0200

    update

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,12 @@
-print("test to publish github")
+--[[
+    To Publish to Github:
+
+    open branches
+    press the 3 dots (...)
+    commit
+    commit and push.
+    
+]]
+
+if (game.PlaceId == 8540346411) then
+end
